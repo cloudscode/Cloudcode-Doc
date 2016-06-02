9bee738a0329ba34f3134564714f97f689416826
Administrator <Administrator@USER-20150923VB>
2016-06-02 13:49:50 +0800

    Signed-off-by: Administrator <Administrator@USER-20150923VB>

--- src/main/java/com/cloudcode/doc/utils/World2PDF.java
+++ src/main/java/com/cloudcode/doc/utils/World2PDF.java
@@ -6,8 +6,12 @@ import java.io.FileOutputStream;
 import java.io.InputStream;
 import java.io.OutputStream;
 
+import org.docx4j.Docx4jProperties;
 import org.docx4j.convert.out.pdf.PdfConversion;
 import org.docx4j.convert.out.pdf.viaXSLFO.PdfSettings;
+import org.docx4j.fonts.IdentityPlusMapper;
+import org.docx4j.fonts.Mapper;
+import org.docx4j.fonts.PhysicalFonts;
 import org.docx4j.openpackaging.packages.WordprocessingMLPackage;
 
 public class World2PDF {
@@ -25,10 +29,17 @@ public class World2PDF {
                     "docx/1.docx"));
             WordprocessingMLPackage wordMLPackage = WordprocessingMLPackage
                     .load(is);
- 
+            Mapper fontMapper = new IdentityPlusMapper();  
+            fontMapper.getFontMappings().put("华文行楷", PhysicalFonts.getPhysicalFonts().get("STXingkai"));  
+            fontMapper.getFontMappings().put("华文仿宋", PhysicalFonts.getPhysicalFonts().get("STFangsong"));  
+            fontMapper.getFontMappings().put("隶书", PhysicalFonts.getPhysicalFonts().get("LiSu"));  
+            fontMapper.getFontMappings().put("宋体", PhysicalFonts.getPhysicalFonts().get("SimSun"));  
+            wordMLPackage.setFontMapper(fontMapper);  
             // 2) Prepare Pdf settings
             PdfSettings pdfSettings = new PdfSettings();
- 
+            Docx4jProperties.getProperties().setProperty("docx4j.Log4j.Configurator.disabled", "true");
+            //Log4jConfigurator.configure();            
+           // org.docx4j.convert.out.pdf.viaXSLFO.Conversion.log.setLevel(Level.OFF);
             // 3) Convert WordprocessingMLPackage to Pdf
             OutputStream out = new FileOutputStream(new File(
                     "pdf/HelloWorld.pdf"));
